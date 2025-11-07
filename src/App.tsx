e1aca5f4315d4bcace1cb57400da0a9742163858
AmanMishra0987 <aman0987mishra@gmail.com>
2025-11-07 12:30:54 +0530

    asdsa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import AddUserPage from './pages/AddUserPage';
